af7cb8848ac2f41eff7be541dd957f6b42e1069f
Prayushh <khadkaprayush8@gmail.com>
2026-07-04 14:43:09 +0545

    Updated dataset.py

--- same_param_training/dataset.py
+++ same_param_training/dataset.py
@@ -1,8 +1,22 @@
+import torch
 from torch.utils.data import DataLoader
-# Code to Load and send the testing and training dataset
+from torchvision import datasets, transforms
 
-def get_loaders(client_id, n_clients, mode, batch_size=32):
+def get_transforms():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+def get_plain_loaders(batch_size=32):
+    train_dir = '../dataset/Epic and CSCR hospital Dataset/Train'
+    test_dir = '../dataset/Epic and CSCR hospital Dataset/Test'
+
+    train_dataset = datasets.ImageFolder(root=train_dir, transform=get_transforms())
+    test_dataset = datasets.ImageFolder(root=test_dir, transform=get_transforms())
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
     
-    train_dataset = datasets.ImageFolder(
-        
-    )
+    return train_loader, test_loader

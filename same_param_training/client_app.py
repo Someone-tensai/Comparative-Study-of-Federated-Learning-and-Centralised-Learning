cf5e0468d93f780dc83ef00e29359f63e63689df
Someone-tensai <paudelsulav5@gmail.com>
2026-07-04 13:55:50 +0545

    Preprocessing and Setup Fix
    Fixed Preprocessing Errors, Setup Model Training , Logs and Validation

--- same_param_training/client_app.py
+++ same_param_training/client_app.py
@@ -1,8 +1,8 @@
 from flwr.app import ArrayRecord, Context, Message, MetricRecord, RecordDict
 from flwr.clientapp import ClientApp
-
+import torch
 from same_param_training.models import our_model
-from same_param_training.dataset import get_test_loader, get_client_loader
+from same_param_training.dataset import  get_client_loader
 from same_param_training.train import train_model, test_model
 
 
@@ -13,30 +13,42 @@ def train(msg: Message, context: Context):
     
     freeze_backbone = context.run_config["freeze-backbone"]
     model_name = context.run_config["model-name"]
-    # Define the Model
+    learning_rate = context.run_config["learning-rate"]
+    weight_decay = context.run_config["weight-decay"]
+    local_epochs = context.run_config["local-epochs"]
+    
     model = our_model(model_name, freeze_backbone)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    
     client_id = context.node_config["partition-id"]+1
     n_clients = context.node_config["num-partitions"]
-    client_loader = get_client_loader(client_id, n_clients, mode="noniid")
+    
+    client_loader, _ = get_client_loader(client_id, n_clients, mode="noniid")
+
     # Get Weights from the Server
     weights = msg.content["arrays"].to_torch_state_dict()
     
     # Set the current model weights to the weights sent by the server
     model.load_state_dict(weights)
        
-    # Train the model weights 
-    train_model(model, client_loader)
+    # Train the model  
+    train_loss = train_model(model, learning_rate, weight_decay, local_epochs, device, client_loader)
     
     # Get New Model Weights
     new_weights = ArrayRecord(model.state_dict())
     
     # Send the weights and metrics back to the server
-    
+    metrics = MetricRecord({
+        "train_loss" : train_loss,
+        "num-examples": len(client_loader.dataset)
+    })
     return Message(
         content=RecordDict({
-            "arrays": ArrayRecord(new_weights),
-            "metrics": MetricRecord({})
-        })
+            "arrays": new_weights,
+            "metrics": metrics, 
+        }), 
+        reply_to=msg
     )
 
 @app.evaluate()
@@ -44,21 +56,26 @@ def evaluate(msg: Message, context: Context):
       
     freeze_backbone = context.run_config["freeze_backbone"]
     model_name = context.run_config["model_name"]
+    
     model = our_model(model_name, freeze_backbone)
-    test_loader = get_test_loader()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    
+    _, val_loader = get_client_loader()
     
     weights = msg.content["arrays"].to_torch_state_dict()
     
     model.load_state_dict(weights)    
     # Evaluate the Model
-    loss, acc = test_model(model, test_loader)
+    loss, acc = test_model(model, device, val_loader)
     
     # Send the evaluation results back
     return Message(
         content=RecordDict({
             "metrics": MetricRecord({
-                "loss": loss,
-                "accuracy": acc
+                "eval_loss": loss,
+                "eval_accuracy": acc,
+                "num-examples": len(val_loader.dataset),
             })
-        })
+        }), reply_to=msg
     )
